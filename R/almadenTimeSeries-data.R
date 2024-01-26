f0b41e003573614693e829cd2845001c1fd3c210
ebrombacher <ebrombacher@web.de>
2024-01-26 13:02:34 +0100

    Added simulated Almaden data

---
+++ R/almadenTimeSeries-data.R
@@ -0,0 +1,25 @@
+#' Simulated time series data of cell classes
+#'
+#' Time series data is modelled using Data2Dynamics and is based on the 
+#' NFkB model of Almaden et al.
+#'  (\href{https://doi.org/10.1016/j.celrep.2014.11.024}).
+#'
+#' @docType data
+#'
+#' @usage data(almadenTimeSeries)
+#'
+#' @keywords datasets
+#'
+#' @references Almaden, Jonathan V., et al. "A pathway switch directs BAFF 
+#' signaling to distinct NFκB transcription factors in maturing and 
+#' proliferating B cells." Cell reports 9.6 (2014): 2098-2111.
+#' (\href{https://doi.org/10.1016/j.celrep.2014.11.024})
+#' 
+#' @references Raue, Andreas, et al. "Data2Dynamics: a modeling environment 
+#' tailored to parameter estimation in dynamical systems." Bioinformatics 
+#' 31.21 (2015): 3558-3560. 
+#' (\href{https://doi.org/10.1093/bioinformatics/btv405})
+#'
+#' @examples
+#' data(almadenTimeSeries)
+"almadenTimeSeries"
