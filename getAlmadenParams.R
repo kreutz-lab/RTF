b8b7633bbcc3f7decf3b6a9fa6c6f6815f0195ca
ebrombacher <ebrombacher@web.de>
2024-02-20 11:09:35 +0100

    Moved files

---
+++ getAlmadenParams.R
@@ -0,0 +1,30 @@
+library(RTF)
+
+data(almadenTimeSeries)
+fileString <- "almaden"
+
+almadenParams <-
+  RTF::getParamsFromMultipleTimeSeries(
+    almadenTimeSeries,
+    fileString = fileString,
+    nInitialGuesses = 400
+  )
+
+write.csv(almadenParams, "almadenParams.csv", row.names = FALSE)
+save(almadenParams, file = "almadenParams.rda", version = 2)
+
+res.lst <- readRDS(file = paste0(fileString, ".RDS"))
+
+RTF::plotRTFForAllTimeSeries(
+  res.lst,
+  fileString = fileString,
+  plotFitsToSingleFile = FALSE,
+  plotFitOnly = TRUE,
+  height = 7,
+  width = 7
+)
+
+session <- sessionInfo()
+sink(paste0("getAlmadenParams_sessionInfo.txt"))
+print(session)
+sink()
