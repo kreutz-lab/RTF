418dac983e105b7c57872310cff2727ed25a35f8
ebrombacher <ebrombacher@web.de>
2024-03-22 11:15:02 +0100

    Added Matsumoto dataset

---
+++ R/matsumoto-data.R
@@ -0,0 +1,20 @@
+#' Experimental time series data with dose dependencies
+#'
+#' Time series data of amino acids showing dose dependencies for the 
+#' ingestion of branched-chain amino acids, taken from the publication of 
+#' Matsumoto et a. 2014. (\href{https://doi.org/10.1186/2193-1801-3-35}).
+#' 
+#' @docType data
+#'
+#' @usage data(matsumoto)
+#'
+#' @keywords datasets
+#'
+#' @references Matsumoto, T., Nakamura, K., Matsumoto, H. et al. Bolus 
+#' ingestion of individual branched-chain amino acids alters plasma amino acid 
+#' profiles in young healthy men. SpringerPlus 3, 35 (2014).
+#' (\href{https://doi.org/10.1186/2193-1801-3-35})
+#'
+#' @examples
+#' data(matsumoto)
+"matsumoto"
