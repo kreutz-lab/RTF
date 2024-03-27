9fd244bebfcfb92595db36799d3d4d85f6ccf484
ebrombacher <ebrombacher@web.de>
2024-03-27 16:24:34 +0100

    Minor changes

--- R/plotParamDistribution.R
+++ R/plotParamDistribution.R
@@ -6,6 +6,8 @@
 #' @param df.parameterValues.long Data frame of the parameter values which
 #' have been determined by each of the fits. Two columnns: parameter name
 #' ('variable'), value of parameter ('value').
+#' @param xLabelSize Numeric value of x-axis label sizes.
+#' @param yLabelSize Numeric value of y-axis label sizes.
 #' @export plotParamDistribution
 #' @examples
 #' parameters <- c("par1", "par2", "par3", "par4", "par5")
@@ -22,7 +24,9 @@
 #'   value = values)
 #' gg.paramDistr <- plotParamDistribution(df.parameterValues.long)
 
-plotParamDistribution <- function(df.parameterValues.long) {
+plotParamDistribution <- function(df.parameterValues.long, 
+                                  xLabelSize = 5,
+                                  yLabelSize = 5) {
   ggplot2::ggplot(df.parameterValues.long, ggplot2::aes(x = value)) +
     ggplot2::geom_histogram(alpha = 0.6) +
     ggplot2::theme_bw() +
@@ -30,7 +34,9 @@ plotParamDistribution <- function(df.parameterValues.long) {
       legend.position = "none",
       panel.spacing = ggplot2::unit(0.1, "lines"),
       strip.text.x = ggplot2::element_text(size = 8),
-      axis.text.x = ggplot2::element_text(angle = 45, hjust = 1)
+      axis.text.y = ggplot2::element_text(size = yLabelSize),
+      axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, 
+                                          size = xLabelSize)
     ) +
     ggplot2::facet_wrap(~variable, scales = "free") +
     ggplot2::xlab("Value") +
